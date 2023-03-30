1430ecf4b17d926934afbddbab88e71f886f619d
maxpol <mpoluyk@gmail.com>
2023-03-30 12:02:58 +0300

    fixture added

--- test_main.py
+++ test_main.py
@@ -9,6 +9,15 @@ test_data = [
 ]
 
 
+@pytest.fixture
+def example_data():
+    return [
+        ['usa', '1000', '12849'],
+        ['ukraine', '18472', '12345'],
+        ['germany', '12345', '28367']
+    ]
+
+
 def test_get_data():
     with open('population.txt', 'w') as f:
         for line in test_data:
