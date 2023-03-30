d646359856219c5fe8e4b9a1e69dd9a2d09aed39
maxpol <mpoluyk@gmail.com>
2023-03-30 11:14:17 +0300

    get_area and area_sort methods added

--- main.py
+++ main.py
@@ -3,3 +3,14 @@ def get_data(filename):
         data = [line.strip().split(',') for line in file]
         return data
 
+
+def get_area(country):
+    return int(country[1])
+
+
+def area_sort(data):
+    sorted_by_area = sorted(data, key=get_area, reverse=True)
+    return sorted_by_area
+
+
+
