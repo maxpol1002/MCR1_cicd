067e71397204406bbf6af5ed42adafe964dbe9a5
maxpol <mpoluyk@gmail.com>
2023-03-30 11:19:59 +0300

    get_population and population_sort methods added

--- main.py
+++ main.py
@@ -8,9 +8,21 @@ def get_area(country):
     return int(country[1])
 
 
+def get_population(country):
+    return int(country[2])
+
+
 def area_sort(data):
     sorted_by_area = sorted(data, key=get_area, reverse=True)
     return sorted_by_area
+
+
+def population_sort(data):
+    sorted_by_population = sorted(data, key=get_population, reverse=True)
+    return sorted_by_population
+
+
+
 
 
 
